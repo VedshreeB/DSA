9cd4c0d229124f33411bbb20c22696f17093cfbd
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-05-21 15:08:26 +0530

    Create DP_6_2DP_EqualSumPartition.cpp

---
+++ DP_6_2DP_EqualSumPartition.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+     bool isSubsetSum(vector<int>arr, int sum){
+        int n = arr.size();
+        int t[n+1][sum+1];
+        
+        //intialization
+        for(int i=0; i<n+1 ; i++){
+            t[i][0] = 1;
+        }
+        //intialization
+        for(int i=1 ; i<sum+1 ; i++)
+            t[0][i] = 0;
+        
+        
+        for(int i=1 ; i<n+1 ; i++){
+            for(int j=1 ; j<sum+1; j++){
+                
+                 if(arr[i-1] <= j){
+                    t[i][j] = (t[i-1][j] ||  t[i-1][j-arr[i-1]]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        
+        long long sum = 0;
+        for(int i=0 ;i<n ; i++)
+            sum += nums[i];
+        
+        if(sum%2 != 0 )
+            return false;
+        else
+            return isSubsetSum(nums,sum/2);      
+        
+    }
+};
