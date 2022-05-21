daa4347925df4f101b0398a1344aa24b0ffa5095
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-05-21 14:41:20 +0530

    Create DP_5_2DP_SubsetSum.cpp

---
+++ DP_5_2DP_SubsetSum.cpp
@@ -0,0 +1,28 @@
+ bool isSubsetSum(vector<int>arr, int sum){
+        int n = arr.size();
+        int t[n+1][sum+1];
+        
+        //intialization
+        for(int i=0; i<n+1 ; i++){
+            t[i][0] = 1;
+        }
+        //intialization
+        for(int i=1 ; i<sum+1 ; i++)
+            t[0][i] = 0;
+        
+        
+        for(int i=1 ; i<n+1 ; i++){
+            for(int j=1 ; j<sum+1; j++){
+                
+                 if(arr[i-1] <= j){
+                    t[i][j] = (t[i-1][j] ||  t[i-1][j-arr[i-1]]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][sum];
+    }
+    
