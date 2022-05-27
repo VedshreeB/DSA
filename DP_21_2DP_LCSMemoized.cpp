b10f39e8a46bc9cfcfff0663a8e1d27acb00c7ac
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-05-27 19:47:59 +0530

    Update DP_21_2DP_LCSMemoized.cpp

--- DP_21_2DP_LCSMemoized.cpp
+++ DP_21_2DP_LCSMemoized.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+    
     int LCS(string x, string y, int n ,int m,vector<vector<int>> &dp){
         //base case
-        if(n < 0 || m < 0 )
-            return 0;
+        if(n == 0 || m == 0 )   //empty string
+            return 0;   
         
-        if(dp[n][m] != -1) return dp[n][m];
+        if(dp[n-1][m-1] != -1) return dp[n-1][m-1];
         
-        if(x[n] == y[m])
-            return dp[n][m] =  1 + LCS(x,y,n-1,m-1,dp);
+        if(x[n-1] == y[m-1])
+            return dp[n-1][m-1] =  1 + LCS(x,y,n-1,m-1,dp);
         
         else 
-            return dp[n][m] = max(LCS(x,y,n,m-1,dp), LCS(x,y,n-1,m,dp));        
+            return dp[n-1][m-1] = max(LCS(x,y,n,m-1,dp), LCS(x,y,n-1,m,dp));        
     }
     int longestCommonSubsequence(string x, string y) {
-    //striver
+       
         int n = x.length();
         int m = y.length();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-                
-        return LCS(x,y,n-1,m-1,dp);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+         
+        return LCS(x,y,n,m,dp);
+        
     }
 };
