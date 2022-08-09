883a668f8fef2b1eabfc6d00559f5b64b66616fd
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-08-09 18:02:29 +0530

    Time: 214 ms (82.50%), Space: 60.7 MB (93.68%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    //in[], out[] //s.c = O(N^2)
+    /*//in[], out[] //s.c = O(2*N)
+     //directed graph ... indegree..outdegree
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> in(n+1,0);
         vector<int> out(n+1,0);
@@ -18,5 +19,28 @@ public:
                 return i;
         }
         return -1;
-    }
+    }*/
+    /*
+     indegree-outdegree = n-1;
+     n-1    -  0 = n-1;
+    */
+    int findJudge(int n, vector<vector<int>>& trust){
+         vector<int> check(n+1,0);
+        
+        for(int i=0 ; i<trust.size() ; i++){
+            int a = trust[i][0];
+            int b = trust[i][1];
+            
+            check[b]++;
+            check[a]--;
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(check[i]==n-1)
+                return i;
+        }
+        return -1;
+        
+    }  
+    //t.c = O(n) , sc = O(n)
 };
