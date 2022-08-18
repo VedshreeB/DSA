b2575429dbbacfb03a195c59c511bbeba8b68e5e
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-08-18 23:18:17 +0530

    Time: 317 ms (38.28%), Space: 60.8 MB (67.91%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-    /*//in[], out[] //s.c = O(2*N)
-     //directed graph ... indegree..outdegree
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> in(n+1,0);
-        vector<int> out(n+1,0);
+        vector<int> d(n+1,0);
         
-        for(int i=0 ; i<trust.size() ; i++){
+        for(int i=0; i<trust.size() ;i++){
             int a = trust[i][0];
             int b = trust[i][1];
             
-            in[b]++;
-            out[a]++;
+            d[a]--;
+            d[b]++;
         }
         
         for(int i=1; i<=n; i++){
-            if(out[i]==0 && in[i]==n-1)
+            if(d[i]==n-1)
                 return i;
         }
         return -1;
-    }*/
-    /*
-     indegree-outdegree = n-1;
-     n-1    -  0 = n-1;
-    */
-    int findJudge(int n, vector<vector<int>>& trust){
-         vector<int> check(n+1,0);
-        
-        for(int i=0 ; i<trust.size() ; i++){
-            int a = trust[i][0];
-            int b = trust[i][1];
-            
-            check[b]++;
-            check[a]--;
-        }
-        
-        for(int i=1; i<=n; i++){
-            if(check[i]==n-1)
-                return i;
-        }
-        return -1;
-        
-    }  
-    //t.c = O(n) , sc = O(n)
+    }
 };
