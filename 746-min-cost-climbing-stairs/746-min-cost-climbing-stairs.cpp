c7732cc304ef21ef21d3f52e0260f64fd70b8b10
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-08-26 16:34:19 +0530

    Time: 13 ms (36.45%), Space: 13.9 MB (26.05%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -19,12 +19,32 @@ public:
         return dp[n] = (cost[n] + min(solve2(cost,n-1,dp),solve2(cost,n-2,dp)));
     }
     
+    //bottom up - tabualtion
+    int solve3(vector<int> &cost,int n){
+        vector<int> dp(n+1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        //2,3,4,5 - n-1
+        for(int i=2; i<n; i++){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+        }
+        
+        //inital call dekho
+        return min(dp[n-1],dp[n-2]);        
+    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         
+        /*
         //STEP 1
         vector<int> dp(n+1,-1);
         int ans = min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
         return ans;
+        */
+        
+        return solve3(cost,cost.size());
+        
     }
 };
