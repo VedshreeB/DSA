840f08527665d850a1eae482022c409521a4d7ff
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-08-28 07:04:45 +0530

    Create DP_Striver_07NinjasTraining.cpp

---
+++ DP_Striver_07NinjasTraining.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n,vector<int>(4));
+    //base case
+    dp[0][0] = max(points[0][1],points[0][2]);
+    dp[0][1] = max(points[0][0],points[0][2]);
+    dp[0][2] = max(points[0][0],points[0][1]);
+    dp[0][3] = max(points[0][0],max(points[0][1],points[0][2]));
+    
+    for(int day=1; day<n; day++){
+        for(int last=0; last<4; last++){
+                dp[day][last] = 0;
+                for(int task=0; task<=2; task++){
+                    if(task!=last){
+                        int pts= points[day][task] + dp[day-1][task];
+                        dp[day][last] = max(dp[day][last], pts);
+                    }
+                }
+        }
+    }
+    return dp[n-1][3];
+}
