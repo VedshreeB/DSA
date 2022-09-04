a169d8ef1172389962fc851751a9077f4c1f2fa7
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-09-04 14:02:02 +0530

    Time: 3 ms (98.04%), Space: 13.1 MB (29.31%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,48 +1,12 @@
 class Solution {
 public:
-    int f(int ind,int buy,vector<int>&prices,vector<vector<int>> &dp){
-       //base case
-        if(ind == prices.size()-1){
-            if(buy == 1)return prices[ind];
-            else return 0;
-        }
-        
-        if(dp[ind][buy]!= -1)return dp[ind][buy];
-        
-        if(buy == 0){
-            int buying = -prices[ind] + f(ind+1,1,prices,dp);
-            int noaction = f(ind+1,buy,prices,dp);
-            return dp[ind][buy] = max(buying ,noaction);
-        }else{
-            int selling = prices[ind] + f(ind+1,0,prices,dp);
-            int noaction = f(ind+1,buy,prices,dp);
-            return dp[ind][buy] = max(selling,noaction);
-        }
-    }
     int maxProfit(vector<int>& prices) {
+        int ans = 0;
         int n = prices.size();
-        //vector<vector<int>> dp(n,vector<int>(2,-1));
-        //return f(0,0,prices,dp);/
-        
-        vector<vector<int>> dp(n,vector<int>(2,0));
-        
-        dp[n-1][1] = prices[n-1];
-        dp[n-1][0] = 0;
-        
-        for(int ind=n-2; ind>=0 ; ind--){
-            for(int buy = 0; buy<2; buy++){
-                
-              if(buy == 0){
-                    int buying = -prices[ind] + dp[ind+1][1];
-                    int noaction = dp[ind+1][buy];
-                    dp[ind][buy] = max(buying ,noaction);
-                }else{
-                    int selling = prices[ind] + dp[ind+1][0];
-                    int noaction = dp[ind+1][buy];
-                    dp[ind][buy] = max(selling,noaction);
-                }
-            }
+        for(int i=1; i<n; i++){
+            if(prices[i] > prices[i-1])
+                ans += prices[i]-prices[i-1];
         }
-        return dp[0][0];
+        return ans;
     }
 };
