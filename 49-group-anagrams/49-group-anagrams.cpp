494fecfd1626be721550ceb6c437f43162fae8b2
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-09-21 00:32:50 +0530

    Time: 88 ms (24.62%), Space: 20.7 MB (41.41%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    /*vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
         map<string,vector<string>> mpp;
         
@@ -14,5 +14,32 @@ public:
             ans.push_back(a.second);    
         }
         return ans;
+    }*/
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            mp[strSort(s)].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }
+private:
+    string strSort(string s) {
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int c = 0; c < 26; c++) {
+            t += string(counter[c], c + 'a');
+        }
+        return t;
     }
 };
+
+
