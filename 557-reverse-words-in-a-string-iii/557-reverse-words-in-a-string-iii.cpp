622ebbb922cea74f1e6a7c01bedf8718d859cb82
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-09-22 10:45:11 +0530

    Time: 45 ms (18.40%), Space: 9.5 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int len = s.length();
+        /*int len = s.length(); //O(1)
         string ans;
         string temp="";
         for(auto it:s){
          
             if(it == ' '){
-                reverse(temp.begin(),temp.end());
-                ans.append(temp);
+                reverse(temp.begin(),temp.end());       //O(|temp|)
+                ans.append(temp);   //O(|temp|)
                 ans += ' ';
                 temp = "";
             }
@@ -20,6 +20,17 @@ public:
         
         reverse(temp.begin(),temp.end());
         ans.append(temp);
-        return ans;
+        return ans;*/
+        
+        int n = s.length();
+        int j=0;
+        
+        for(int i=0;i<=n;i++){
+            if(s[i]==' '|| s[i]=='\0'){
+                reverse(s.begin()+j,s.begin()+i);
+                j = i+1;
+            }
+        }
+        return s;
     }
 };
