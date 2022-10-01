4f33d18f7dba9b27c76d8e908dfd0f6513a76bc4
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2022-10-01 10:26:18 +0530

    Time: 2 ms (67.17%), Space: 6.2 MB (75.45%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int solve(int i,string &s,int *dp){
-        if(i>=s.length())return 1;
-        if(dp[i]!=-1)return dp[i];
-        
-        int ans = 0;
-        int op1=s[i]-'0',op2=0;
-        if(i<s.length()-1){
-            op2 = op1*10 + s[i+1]-'0';
-        }
-        if(op1>0)ans += solve(i+1,s,dp);
-        if(op1>0 && op2>0 && op2<=26) ans+=solve(i+2,s,dp);
-        return dp[i]=ans;
-    }
     int numDecodings(string s) {
-        int dp[101];
-        memset(dp,-1,sizeof dp);
-        return solve(0,s,dp);
+        int dp[102];
+        memset(dp,0,sizeof dp);
+        int n = s.length();
+        dp[n]=1;
+        dp[n+1]=1;
+        
+        for(int i=n-1; i>=0 ; i--){
+            
+            int ans = 0;
+            int op1=s[i]-'0',op2=0;
+            if(i<n-1){
+                op2 = op1*10 + s[i+1]-'0';
+            }
+            if(op1>0)ans += dp[i+1];
+            if(op1>0 && op2>0 && op2<=26) ans+=dp[i+2];
+            dp[i]=ans;
+        }
+        
+        return dp[0];
     }
 };
