ae8249ee607070ffc68eeddc5f752810f72c29dd
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2025-05-18 12:53:28 +0530

    Update GraphRepresentationAsList.java

--- Java/Graph/GraphRepresentationAsList.java
+++ Java/Graph/GraphRepresentationAsList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Main {
     //implementation of graph representation as adjancency list in java
   public static void main(String[] args) {
@@ -5,16 +7,17 @@ class Main {
     //intialization
     int v=5;
     for(int i=0; i<=v ; i++ ){
-        adj.add(new ArrayList<Integer>);
+        adj.add(new ArrayList<Integer>());
     }
     
+    //adding an edge for undirected graph
     //edge 1-->2
     adj.get(1).add(2);
+    adj.get(2).add(1);
+    
+    
+    //adding edge for directed graph
     //edge 1--->3
     adj.get(1).add(3);
-    
-    
-    //edge 2--->4
-    adj.get(2).add(4);
   }
 }
