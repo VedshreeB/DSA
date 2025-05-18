185802e82ed16455c439b829e5d0125d7726d05a
VedshreeB <74200552+VedshreeB@users.noreply.github.com>
2025-05-18 12:50:36 +0530

    Create GraphRepresentationAsList.java

---
+++ Java/Graph/GraphRepresentationAsList.java
@@ -0,0 +1,20 @@
+class Main {
+    //implementation of graph representation as adjancency list in java
+  public static void main(String[] args) {
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+    //intialization
+    int v=5;
+    for(int i=0; i<=v ; i++ ){
+        adj.add(new ArrayList<Integer>);
+    }
+    
+    //edge 1-->2
+    adj.get(1).add(2);
+    //edge 1--->3
+    adj.get(1).add(3);
+    
+    
+    //edge 2--->4
+    adj.get(2).add(4);
+  }
+}
